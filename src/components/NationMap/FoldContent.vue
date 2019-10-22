82d496e7a3c57df58b6fe7ececa5722b83f9cf61
ZhangYanzhe <2712885306@qq.com>
2019-10-22 22:25:26 +0800

    h

--- src/components/NationMap/FoldContent.vue
+++ src/components/NationMap/FoldContent.vue
@@ -201,22 +201,24 @@ export default {
     },
     drawPie: function () {
       let echartsPie = echarts.init(document.getElementById('echartsPie'))
-      echartsPie.setOption({
+      let option = {
         series: [
           {
             name: '访问来源',
             type: 'pie',
             radius: '55%',
+            center: ['50%', '50%'],
             data: [
-              { value: 235, name: '视频广告' },
-              { value: 274, name: '联盟广告' },
-              { value: 310, name: '邮件营销' },
               { value: 335, name: '直接访问' },
+              { value: 310, name: '邮件营销' },
+              { value: 274, name: '联盟广告' },
+              { value: 235, name: '视频广告' },
               { value: 400, name: '搜索引擎' }
             ]
           }
         ]
-      })
+      }
+      echartsPie.setOption(option)
     }
   },
   mounted () {
